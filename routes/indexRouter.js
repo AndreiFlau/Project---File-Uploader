5e34f5a691059eb3c8f8f62b6f399fb9fdd4fa56
Andrei <andreiflausino@live.com>
2024-08-15 21:17:22 -0300

    added the ability to log-out. fixed index page requiring user to be logged in

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,8 +3,17 @@ const { getFoldersQuery } = require("../db/queries");
 const indexRouter = Router();
 
 indexRouter.get("/", async (req, res) => {
-  const folders = await getFoldersQuery(req.user.id);
+  const folders = req.user && req.user.id ? await getFoldersQuery(req.user.id) : [];
   res.render("index", { user: req.user, folders: folders });
+});
+
+indexRouter.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = indexRouter;
